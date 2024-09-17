eec8a4087c484d33cfe3a6c05f102792dd743514
Bhuwan Pandit <bhuwanpandit109@gmail.com>
2024-09-17 23:19:54 +0100

    feat: ignore meaningless tokens while tokenization

--- src/com/craftinginterpreters/lox/Scanner.java
+++ src/com/craftinginterpreters/lox/Scanner.java
@@ -80,6 +80,13 @@ class Scanner {
                     addToken(SLASH);
                 }
                 break;
+            case ' ':
+            case '\r':
+            case '\t':
+                break;
+            case '\n':
+                line++;
+                break;
             default:
                 Lox.error(line, "Unexpected character.");
                 break;
