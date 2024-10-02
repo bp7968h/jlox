b0fb8b4fc85f2ca7cab0b5e0aa4d2ee8b211ac30
Bhuwan Pandit <bhuwanpandit109@gmail.com>
2024-10-02 22:59:54 +0100

    feat: add support for while statement

--- src/lox/Interpreter.java
+++ src/lox/Interpreter.java
@@ -161,6 +161,14 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         return null;
     }
 
+    @Override
+    public Void visitWhileStmt(Stmt.While stmt) {
+        while (isTruthy(evaluate(stmt.condition))) {
+            execute(stmt.body);
+        }
+        return null;
+    }
+
     void executeBlock(List<Stmt> statements,
             Environment environment) {
         Environment previous = this.environment;
